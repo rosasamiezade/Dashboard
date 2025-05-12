4e3da2aabe3f4cb354efdb0aa32ef3308e8c468c
rosasamiezade <rozasamizade@gmail.com>
2025-05-12 18:48:05 +0330

    refactor : delete custom hooks

--- src/hooks/useTotalProducts.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getTotalProductsAmount } from "../services/api";
-
-export const useTotalProducts = () => {
-  return useQuery({
-    queryKey: ["total-products"],
-    queryFn: getTotalProductsAmount,
-  });
-};
